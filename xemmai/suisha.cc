293755db5b49925c285bd2ed00d6fe10d32e63a7
Shin-ichi MORITA <shin1morita@gmail.com>
2022-05-15 07:35:24 +0900

    Updated the required xemmai version.

--- xemmai/suisha.cc
+++ xemmai/suisha.cc
@@ -61,9 +61,9 @@ std::vector<std::pair<t_root, t_rvalue>> t_library::f_define()
 	t_type_of<t_timer>::f_define(this);
 	t_type_of<t_loop>::f_define(this);
 	return t_define(this)
-		(L"Wait"sv, t_object::f_of(v_type_wait))
-		(L"Timer"sv, t_object::f_of(v_type_timer))
-		(L"Loop"sv, t_object::f_of(v_type_loop))
+		(L"Wait"sv, static_cast<t_object*>(v_type_wait))
+		(L"Timer"sv, static_cast<t_object*>(v_type_timer))
+		(L"Loop"sv, static_cast<t_object*>(v_type_loop))
 		(L"main"sv, t_static<void(*)(t_library*, const t_pvalue&), f_main>())
 		(L"loop"sv, t_static<t_pvalue(*)(), f_loop>())
 	;
